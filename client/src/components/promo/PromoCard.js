8c878190cb695bbbfca9634ba3fc0949279caf02
cybersyn <cybersyn@gmail.com>
2020-06-26 14:46:45 -0700

    Add new animation to promoCard with react-transition-group

--- client/src/components/promo/PromoCard.js
+++ client/src/components/promo/PromoCard.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import EventLocationMap from '../EventLocationMap';
 import { AiOutlineCalendar } from 'react-icons/ai';
 import { RiMapPin2Line } from 'react-icons/ri';
 import { GoChevronDown, GoChevronUp } from 'react-icons/go';
 import ImageCarousel from '../ImageCarousel';
+import spinner from '../../assets/spinner.gif';
 
 const PromoCard = ({ promoState }) => {
 	//destructure promoState prop
@@ -28,16 +30,20 @@ const PromoCard = ({ promoState }) => {
 	} = promoState;
 
 	const [isExpanded, setIsExpanded] = useState(false);
-	
+
 	const [coords, setCoords] = useState({
 		lat: '',
 		lng: '',
 	});
-	
+	const [entered, setEntered] = useState(true);
+
 	const formattedTime = new Date(startDateTime).toLocaleDateString();
-	
-	const expand = () => setIsExpanded(!isExpanded);
-	
+
+	const expand = () => {
+		setEntered(!entered);
+		setIsExpanded(!isExpanded);
+	};
+
 	const picturesArr = pictures
 		.replace(/\n/g, ' ')
 		.split(' ')
@@ -46,7 +52,6 @@ const PromoCard = ({ promoState }) => {
 	const handleSetCoords = (newCoords) => {
 		setCoords(newCoords);
 	};
-
 
 	//need to make api call to opencagedata to convert location address into coordinates for leaflet maps.
 	const getCoords = async () => {
@@ -74,93 +79,136 @@ const PromoCard = ({ promoState }) => {
 		});
 	}, []);
 
-	//if the promoCard is not expanded
-	if (!isExpanded) {
-		return (
-			<div className='p-5'>
-				<div className=' flex-col mx-auto m-5 rounded bg-white shadow-lg text-center'>
-					<h1 className='rounded-t justify-center bg-blue-400 text-center text-white text-2xl break-words'>
-						<p>{name}</p>
-					</h1>
-					<img src={picturesArr[0]} className='w-8/12 mx-auto m-2' />
-					<span className='text-xs text-white p-2 bg-blue-400 rounded'>
-						{type}
-					</span>
-					<div className='flex justify-center'>
-						<div className='p-5'>
-							<AiOutlineCalendar className='text-4xl mx-auto text-blue-400' />
-							<p className='text-xs font-hairline text-opacity-50'>
-								{formattedTime}
-							</p>
-						</div>
-						<div className='p-5'>
-							<RiMapPin2Line className='text-4xl mx-auto text-blue-400' />
-							<p className='text-xs font-hairline text-opacity-50 '>
-								{address}
-							</p>
-						</div>
-					</div>
-					<div
-						className='break-words text-sm m-1 p-1'
-						dangerouslySetInnerHTML={{
-							__html: description.substring(0, 50) + '...',
-						}}
-					></div>
-					<button className='p-1 text-4xl text-blue-400' onClick={expand}>
-						{isExpanded ? <GoChevronUp /> : <GoChevronDown />}
-					</button>
-				</div>
-			</div>
-		);
-	} else if (isExpanded && promoState.description) {
-		//if the promoCard is expanded
-		return (
-			<div className='m-5'>
-				<div className='flex-col mx-auto m-5 rounded bg-white shadow-lg text-center'>
-					<h1 className='rounded-t bg-blue-400 text-center text-white text-3xl break-words'>
-						<p>{name}</p>
-						<button className='p-2' onClick={expand}>
-							{isExpanded ? <GoChevronUp /> : <GoChevronDown />}
-						</button>
-					</h1>
-					<div className='mx-auto'>
-						<ImageCarousel picturesArr={picturesArr} />
-						<EventLocationMap coords={coords} />
-					</div>
-					<span className='text-xs text-white p-2 bg-blue-400 rounded'>
-						{type}
-					</span>
-					<div className='flex justify-around items-center text-center'>
-						<div className='p-2 flex flex-col items-center'>
-							<AiOutlineCalendar className='text-4xl text-blue-400' />
-							<p className='text-sm font-hairline text-opacity-50 '>
-								{formattedTime}
-							</p>
-						</div>
-						<div className='p-1'></div>
-						<div className='p-2 flex flex-col items-center'>
-							<RiMapPin2Line className='text-4xl text-blue-400' />
-							<p className='text-sm font-hairline text-opacity-50'>{address}</p>
-						</div>
-					</div>
+	return (
+		<div>
+			<SwitchTransition mode={'out-in'}>
+				<CSSTransition
+					key={entered}
+					addEndListener={(node, done) => {
+						node.addEventListener('transitionend', done, false);
+					}}
+					classNames='fade'
+					timeout={{
+						appear: 500,
+						enter: 900,
+						exit: 900,
+					}}
+					appear
+					unmountOnExit
+				>
+					{() => {
+						return (
+							<div>
+								{(() => {
+									switch (isExpanded) {
+										case false:
+											return (
+												<div className='p-5'>
+													<div className=' flex-col mx-auto m-5 rounded bg-white shadow-lg text-center'>
+														<h1 className='rounded-t justify-center bg-blue-400 text-center text-white text-2xl break-words'>
+															<p>{name}</p>
+														</h1>
+														<img
+															src={picturesArr[0]}
+															className='w-8/12 mx-auto m-2'
+														/>
+														<span className='text-xs text-white p-2 bg-blue-400 rounded'>
+															{type}
+														</span>
+														<div className='flex justify-center'>
+															<div className='p-5'>
+																<AiOutlineCalendar className='text-4xl mx-auto text-blue-400' />
+																<p className='text-xs font-hairline text-opacity-50'>
+																	{formattedTime}
+																</p>
+															</div>
+															<div className='p-5'>
+																<RiMapPin2Line className='text-4xl mx-auto text-blue-400' />
+																<p className='text-xs font-hairline text-opacity-50 '>
+																	{address}
+																</p>
+															</div>
+														</div>
+														<div
+															className='break-words text-sm m-1 p-1'
+															dangerouslySetInnerHTML={{
+																__html: description.substring(0, 50) + '...',
+															}}
+														></div>
+														<button
+															className='p-1 text-4xl text-blue-400'
+															onClick={expand}
+														>
+															{isExpanded ? <GoChevronUp /> : <GoChevronDown />}
+														</button>
+													</div>
+												</div>
+											);
+										case true:
+											return (
+												<div className='m-5'>
+													<div className='flex-col mx-auto m-5 rounded bg-white shadow-lg text-center'>
+														<h1 className='rounded-t bg-blue-400 text-center text-white text-3xl break-words'>
+															<p>{name}</p>
+															<button className='p-2' onClick={expand}>
+																{isExpanded ? (
+																	<GoChevronUp />
+																) : (
+																	<GoChevronDown />
+																)}
+															</button>
+														</h1>
+														<div className='mx-auto'>
+															<ImageCarousel picturesArr={picturesArr} />
+															{/* <EventLocationMap coords={coords} /> */}
+															{coords.lng && coords.lat !== '' ? (
+																<EventLocationMap coords={coords} />
+															) : (
+																<img src={spinner} />
+															)}
+														</div>
+														<span className='text-xs text-white p-2 bg-blue-400 rounded'>
+															{type}
+														</span>
+														<div className='flex justify-around items-center text-center'>
+															<div className='p-2 flex flex-col items-center'>
+																<AiOutlineCalendar className='text-4xl text-blue-400' />
+																<p className='text-sm font-hairline text-opacity-50 '>
+																	{formattedTime}
+																</p>
+															</div>
+															<div className='p-1'></div>
+															<div className='p-2 flex flex-col items-center'>
+																<RiMapPin2Line className='text-4xl text-blue-400' />
+																<p className='text-sm font-hairline text-opacity-50'>
+																	{address}
+																</p>
+															</div>
+														</div>
 
-					<div
-						className='break-words m-2 p-2 text-center'
-						dangerouslySetInnerHTML={{ __html: description }}
-					></div>
-					<Link to={`/events/${name}`}>See Full Page</Link>
-					<button
-						className='p-2 text-center text-4xl text-blue-400'
-						onClick={expand}
-					>
-						{isExpanded ? <GoChevronUp /> : <GoChevronDown />}
-					</button>
-				</div>
-			</div>
-		);
-	} else {
-		return null;
-	}
+														<div
+															className='break-words m-2 p-2 text-center'
+															dangerouslySetInnerHTML={{ __html: description }}
+														></div>
+														<Link to={`/events/${name}`}>See Full Page</Link>
+														<button
+															className='p-2 text-center text-4xl text-blue-400'
+															onClick={expand}
+														>
+															{isExpanded ? <GoChevronUp /> : <GoChevronDown />}
+														</button>
+													</div>
+												</div>
+											);
+									}
+								})()}
+							</div>
+						);
+					}}
+				</CSSTransition>
+			</SwitchTransition>
+		</div>
+	);
 };
 
 export default PromoCard;
