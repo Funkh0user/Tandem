af5a75a392e83701a6066901a25fa3fee5cbcd7c
cybersyn <cybersyn@gmail.com>
2020-09-11 23:30:36 -0700

    update comments

--- routes/events.js
+++ routes/events.js
@@ -14,10 +14,12 @@ const s3 = new aws.S3({
 	secretAccessKey: process.env.AWS_S3_BUCKET_SECRET,
 });
 
-//a function which takes an image file path, and the name the image will be saved as, and saves the image to aws
+//a function which takes an image file path, and the name the image will be saved as, and saves the image to aws.
+//function will return a promise we create and resolve with the URLs of our images
 const uploadSingleImage = async (imagePath, nameToApply) => {
 	//use the filesystem api provided by node to prepare the image to be uploaded.
 	const imageData = fs.readFileSync(imagePath);
+	//parameters for s3.upload()
 	const params = {
 		Bucket: 'tand3m',
 		Key: nameToApply,
@@ -37,6 +39,7 @@ const uploadSingleImage = async (imagePath, nameToApply) => {
 };
 
 //multer configuration for local disk storage
+// TODO change to not rely on disk storage but instead use memoryStorage.
 // const storage = multer.memoryStorage()
 const storage = multer.diskStorage({
 	destination: __dirname + '/media/',
@@ -51,14 +54,13 @@ const upload = multer({
 	limits: {
 		filesize: 1028 * 1028,
 	},
-	//TODO improve error message / validation for number of pictures and size of pictures use fileFilter: (req, file, cb )=> {}
 
+	//TODO improve error message / validation for number of pictures and size of pictures use fileFilter: (req, file, cb )=> {}
 });
 
 //@route    api/events
 //@description    POST new event
 router.post('/', upload.array('file', 4), async (req, res, next) => {
-	console.log('here are the files you sent to the server', req.files);
 	//get the files paths on the server which we saved via multer from the post request.
 	const imagePaths = req.files;
 	//trim whitespace from eventName and make the name lowercase so its easier to handle later in the get route
@@ -75,7 +77,7 @@ router.post('/', upload.array('file', 4), async (req, res, next) => {
 		return imageLocationOnS3.Location;
 	});
 
-	//Handle our array of promises returned from S3 with Promise.All and save event to mongoDB in the callback.
+	//Handle our array of promises returned from S3 with Promise.All and save event to mongoDB in the callback after all the promises have resolved.
 	Promise.all(uploadedImageData).then(async (imageUrls) => {
 		console.log('heres the result of promise.all', imageUrls);
 
@@ -101,7 +103,7 @@ router.post('/', upload.array('file', 4), async (req, res, next) => {
 				picturesArr: imageUrls,
 			});
 			newEvent.save();
-			res.status(200).json({imageUrls: imageUrls});
+			res.status(200).json({ imageUrls: imageUrls });
 		} catch (error) {
 			console.log('there was an error posting this event', error);
 			res.status(500).send('Server error.');
@@ -110,7 +112,10 @@ router.post('/', upload.array('file', 4), async (req, res, next) => {
 });
 
 //@Route  api/events
-//@description  GET events to display on mainpage in promo widget / cards.
+//@description GET events to display on mainpage
+//Grabs events in multiples of six starting with most recently posted event and serves to front end to render to mainpage
+//no idea if this is actually how we are going to serve events up in the long run, just wanted a way to save events to a server and 
+//get them back to the front end to test components.
 
 router.get('/:number', async (req, res) => {
 	const numberOfEventsToDisplay = req.params.number;
@@ -129,7 +134,7 @@ router.get('/:number', async (req, res) => {
 });
 
 //@Route  api/events
-//@description  GET a specific event to display on its own individual page.
+//@description GET a specific event to display on its own individual page.
 
 router.get('/event/:eventName', async (req, res) => {
 	//formatt url parameter into usable name name before searching mongo db
