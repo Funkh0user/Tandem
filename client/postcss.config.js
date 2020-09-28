aec2a4f28a0230d84aafb2cccef8513cea3a61c6
cybersyn <cybersyn@gmail.com>
2020-09-27 22:41:23 -0700

    fixes issues with tailwind in production mode

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -4,7 +4,9 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     // Specify the paths to all of the template files in your project
     content: [
       './public/**/*.html',
+      './src/**/*.html',
       './src/**/*.js',
+      './src/**/*.jsx',
       // etc.
     ],
   
---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: [
+      './src/**/*.html',
+      './src/**/*.js',
+      './src/**/*.vue',
+      './src/**/*.jsx',
+    ],
+    theme: {},
+    variants: {},
+    plugins: [],
+  }
